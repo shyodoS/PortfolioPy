1292f4229a8f436c002016127cdc8f5695d357d9
shyodos <marceloshyodos@gmail.com>
2025-09-01 11:38:14 -0300

    atualizando requeriments

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\marceloshyodos\Desktop\cvPy\venv
+set VIRTUAL_ENV=C:\Users\marceloshyodos\Documents\portPy\PortfolioPy\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
